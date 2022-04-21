1d4a1d042237d167abc41f144436e4961ba3e7c1
Léo Boyer <contact@leoboyer.fr>
2022-04-21 15:48:29 +0200

    🚀 initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+// vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'lib/main.ts'),
+            name: 'Paapi',
+            fileName: (format) => `paapi.${format}.js`
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['socket.io-client'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+
+                }
+            }
+        }
+    }
+})
