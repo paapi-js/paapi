0b450dfbbd55a7868733d29f4b59fd434a913ba3
Léo Boyer <contact@leoboyer.fr>
2022-04-23 11:30:39 +0200

    New fluent instanciation

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,6 @@
 import {io} from 'socket.io-client';
 import Link from './Link';
 
-export default async function paapi (server: string = 'https://paapi.pnk.network/', id: string|null): Promise<Link> {
-
-    const link: Link = new Link(io(server))
-    await link.waitFor('connect')
-    await link.pair(id)
-    return link
+export default function paapi (server: string = 'https://paapi.pnk.network/'): Link {
+    return new Link(io(server))
 }
